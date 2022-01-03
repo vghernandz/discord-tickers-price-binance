3c50e6043d1b6a6e0fe79596060171c40b9c74bf
vghernandz <vgabriel.hernandz@gmail.com>
2022-01-02 21:34:22 -0500

    get-price-api-axios

--- src/binance/index.js
+++ src/binance/index.js
@@ -1,8 +1,12 @@
 const schedule = require('node-schedule')
+const axios = require('axios')
+const ticker = process.env.TICKER
 
 const pricesGet = async () => {
   schedule.scheduleJob('*/5 * * * * *', async () => {
-    console.log('call......')
+    const {data} = await axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=${ticker}`)
+
+    console.log(data)
   })
 }
 
