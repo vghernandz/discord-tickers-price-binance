05561a452d99f61304aad5a7a1b638c8eb027c64
vghernandz <vgabriel.hernandz@gmail.com>
2022-01-02 21:42:56 -0500

    price-show

--- src/binance/index.js
+++ src/binance/index.js
@@ -1,12 +1,24 @@
 const schedule = require('node-schedule')
 const axios = require('axios')
 const ticker = process.env.TICKER
+const tickerDecimal = process.env.TICKER_DECIMAL
 
 const pricesGet = async () => {
   schedule.scheduleJob('*/5 * * * * *', async () => {
     const {data} = await axios.get(`https://api.binance.com/api/v3/ticker/24hr?symbol=${ticker}`)
 
-    console.log(data)
+    const { lastPrice, priceChangePercent } = data 
+
+      const arrow = priceChangePercent.includes('-') ? '⬋' : '⬈'
+      const stringPrice = `$${Number(lastPrice).toFixed(tickerDecimal)}`
+      const stringChange = `${arrow}${priceChangePercent}%`
+
+      // await discordChangeMessage(`${stringPrice} ${stringChange}`)
+
+    console.log({
+      stringPrice,
+      stringChange
+    })
   })
 }
 
